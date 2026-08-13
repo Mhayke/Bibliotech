4101ae2f08b3c05855db17c1f1e7ff31538e5db9
Thiago <thiagogabriel1904@gmail.com>
2026-08-13 16:55:05 -0300

    feature cadastro-usuarios

---
+++ Downloads/bibliotech-feature-devolucoes/bibliotech-feature-devolucoes/features/usuarios.py
@@ -0,0 +1,24 @@
+usuarios = []
+
+
+
+def cadastrar_usuario():
+    print("\n--- Cadastro de Usuário ---")
+
+    nome = input("Nome do usuário: ")
+    identificador = input("Identificador do usuário: ")
+
+    # Verifica se o identificador já existe
+    for usuario in usuarios:
+        if usuario["identificador"] == identificador:
+            print("Erro: já existe um usuário com esse identificador.")
+            return
+
+    usuario = {
+        "nome": nome,
+        "identificador": identificador
+    }
+
+    usuarios.append(usuario)
+
+    print("Usuário cadastrado com sucesso!")
